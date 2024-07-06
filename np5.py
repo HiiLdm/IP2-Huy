5412982fcd7d7043dbd9a71b5a671745076f7a52
Hiildm <huy.leh28042002@hcmut.edu.vn>
2024-07-06 14:37:58 +0700

    ViPy_np5

---
+++ np5.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.arange(9).reshape(3, 3)
+print("Original array:")
+print(arr)
+
+# Swap column 0 with column 1
+arr[:, [0, 1]] = arr[:, [1, 0]]
+
+print("\nArray after swapping column 0 with column 1:")
+print(arr)
